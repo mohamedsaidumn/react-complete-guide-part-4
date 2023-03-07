7a9edd5bda9dd9b49f433994377cd7d8a16e554d
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-06 21:09:14 -0600

    starter skeleton for login and logut functionality

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Home.module.css";
+
+interface HomeProps {
+  onLogout: () => void;
+}
+const Home = (props: HomeProps) => {
+  return (
+    <Card className={classes.home}>
+      <h1>Welcome back!</h1>
+    </Card>
+  );
+};
+
+export default Home;
---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Navigation from "./Navigation";
+import classes from "./MainHeader.module.css";
+
+interface MainHeaderProps {
+  isAuthenticated: boolean;
+  onLogout: () => void;
+}
+
+const MainHeader = (props: MainHeaderProps) => {
+  return (
+    <header className={classes["main-header"]}>
+      <h1>A Typical Page</h1>
+      <Navigation
+        isLoggedIn={props.isAuthenticated}
+        onLogout={props.onLogout}
+      />
+    </header>
+  );
+};
+
+export default MainHeader;
